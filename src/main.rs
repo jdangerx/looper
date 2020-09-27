41af6c235871764d7b6899c7c147924b3cfb5209
John Xia <john.danger.xia@gmail.com>
2020-09-27 19:27:16 -0400

    Wow, learned how to make an HTTP request. Now to extract anything useful at all...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate reqwest;
+
+use std::env;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let video_id = &args[1].trim();
+    let res = request(&video_id).expect("couldn't request video ID!");
+
+    println!("Got response: {}", res);
+}
+
+fn request(video_id: &str) -> Result<String, Box<dyn Error>> {
+    let url = format!("https://youtube.com/get_video_info?video_id={}", video_id);
+    println!("Requesting: {}", url);
+    Ok(reqwest::blocking::get(&url)?.text()?)
 }
